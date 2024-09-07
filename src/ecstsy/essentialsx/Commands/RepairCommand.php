ebc94749011245bbf380e4fcf4bc83f84d92cd80
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-09-07 15:42:06 -0700

    Added 3 new commands
    added one subcommand

---
+++ src/ecstsy/essentialsx/Commands/RepairCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ecstsy\essentialsx\Commands;
+
+use CortexPE\Commando\BaseCommand;
+use ecstsy\EssentialsX\Commands\SubCommands\Repair\RepairAllSubCommand;
+use ecstsy\essentialsx\Loader;
+use pocketmine\command\CommandSender;
+use pocketmine\item\Durable;
+use pocketmine\item\VanillaItems;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat as C;
+
+class RepairCommand extends BaseCommand {
+
+    public function prepare(): void {
+        $this->setPermission($this->getPermission());
+
+        $this->registerSubCommand(new RepairAllSubCommand(Loader::getInstance(), "all", "Repair all items in your inventory."));
+    }
+
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
+    {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(C::colorize("&r&cError: &4Only in-game players can use &cfix&c."));
+            return;
+        }
+
+        $item = $sender->getInventory()->getItemInHand();
+        $config = Loader::getInstance()->getLang();
+
+        if ($item->equals(VanillaItems::AIR()) || !$item instanceof Durable) {
+            $sender->sendMessage(C::colorize($config->getNested("repair.invalid")));
+            return;
+        }
+
+        if ($item instanceof Durable && $item->getDamage() > 0) {
+            $item->setDamage(0);
+            $sender->getInventory()->setItemInHand($item);
+            $sender->sendMessage(C::colorize(str_replace("{item}", $item->getVanillaName(), $config->getNested("repair.success"))));
+        } else {
+            $sender->sendMessage(C::colorize($config->getNested("repair.invalid")));
+        }
+    }
+
+    public function getPermission(): string {
+        return "essentialsx.repair";
+    }
+}
---
+++ src/ecstsy/essentialsx/Commands/TopCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ecstsy\EssentialsX\Commands;
+
+use CortexPE\Commando\BaseCommand;
+use ecstsy\essentialsx\Loader;
+use pocketmine\command\CommandSender;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\world\Position;
+
+class TopCommand extends BaseCommand {
+
+    public function prepare(): void {
+        $this->setPermission($this->getPermission());
+    }
+
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
+    {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(C::colorize("&r&cError: &4Only in-game players can use &ctop&4."));
+            return;
+        }
+
+        $position = $sender->getPosition();
+        $world = $sender->getWorld();
+        $highestY = $world->getHighestBlockAt($position->x, $position->z);
+        $config = Loader::getInstance()->getLang();
+
+        $sender->teleport(new Vector3($position->x, $highestY, $position->z));
+        $sender->sendMessage(C::colorize($config->getNested("top.success")));
+    }
+
+    public function getPermission(): string {
+        return "essentialsx.top";
+    }
+}
